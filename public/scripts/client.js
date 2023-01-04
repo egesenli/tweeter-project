c35b4a0100f1d829e1a27e0b06a188097d5ac18d
egesenli <egesenli@gmail.com>
2023-01-03 21:52:14 -0500

    Implement functions for showing/hiding error msg

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,50 +7,61 @@
 
 $(document).ready(function () {
 
-  // Function to escape some text, and then use it inside .html() or $().
+  // Function to escape some text, and then use it inside .html() or $()
   const escape = str => {
     const div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   }
 
+  // Show the error message
+  const showErrorMessage = (message) => {
+    $('#nt-error-msg').text(message);
+    $('#new-tweet-error').show();
+  };
+
+  // Hide the error message
+  const hideErrorMessage = () => {
+    $('#new-tweet-error').hide();
+    $('#nt-error-msg').text('');
+  };
+
+  // Function to create a tweet element on the timeline
   const createTweetElement = function (tweetData) {
     const $tweet = $(`<article class="tweet">
     <header class="container-row">
     <div class="profile container-row">
     <img class="th-header img" src=${escape(tweetData.user.avatars)} >
-      <p>${escape(tweetData.user.name)}</p>
+      <p id="username-position">${escape(tweetData.user.name)}</p>
     </div>
     <p class="handle">${escape(tweetData.user.handle)}</p>
     </div>
-  </header> 
-<section class="tweet-content">
-<p>${escape(tweetData.content.text)} </p>
-</section>
-<footer>
-<p class="time-stamp">${timeago.format(tweetData.created_at)}</p> 
-<div>
-<i class="fa-solid fa-flag"></i>
-<i class="fa-solid fa-retweet"></i>
-<i class="fa-solid fa-heart"></i>
-</div>
-</footer>
-</article>`)
+    </header>
+    <section class="tweet-content">
+      <p>${escape(tweetData.content.text)} </p>
+    </section>
+    <footer>
+      <p class="time-stamp">${timeago.format(tweetData.created_at)}</p> 
+    <div>
+     <i class="fa-solid fa-flag"></i>
+    <i class="fa-solid fa-retweet"></i>
+    <i class="fa-solid fa-heart"></i>
+    </div>
+    </footer>
+    </article>`)
     return $tweet;
   }
 
-  const renderTweets = function (tweets) {
-    // Reverse the order of the tweets
+  // Function to render tweets on the timeline. Loops through tweets. Calls createTweetElement for each tweet. Takes return value and appends it to the tweets container
+  const renderTweets = (tweets) => {
+    // Sorts tweets by most recent
     tweets.reverse();
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
     $('#tweets-container').empty();
     for (const tweet of tweets) {
       $('#tweets-container').append(createTweetElement(tweet));
     }
   }
-
+  // Function to load tweets from the database and render them on the timeline using AJAX
   const tweetLoader = async () => {
     try {
       const response = await $.ajax({
@@ -63,7 +74,7 @@ $(document).ready(function () {
     }
   };
 
-  $(document).ready(async function () {
+  $(document).ready(async function() {
     await tweetLoader();
     $('.new-tweet form').submit(async function (event) {
       event.preventDefault();
@@ -71,22 +82,23 @@ $(document).ready(function () {
       // Validation checks
       const tweetContent = $(this).find('textarea').val().trim();
       if (tweetContent === "" || tweetContent === null) {
-        alert("Please enter some tweet content");
+        showErrorMessage('Please enter a tweet before tweeting!');
         return;
       }
       if (tweetContent.length > 140) {
-        alert('Tweet cannot be more than 140 characters');
+        showErrorMessage('Please keep tweet within 140 characters in length!')
         return;
       }
 
       console.log('New tweet submitted');
       try {
-        const response = await $.ajax({
+        await $.ajax({
           type: 'POST',
           url: '/tweets',
           data: $(this).serialize(),
         });
         tweetLoader();
+        hideErrorMessage();
       } catch (error) {
         console.error(error);
       }
